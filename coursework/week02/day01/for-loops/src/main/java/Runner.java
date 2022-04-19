6240fd2e406b1e7d2963c2eb418ffa1321b4aef6
sujangurung <sujangurung@sujangurungsmbp.home>
2022-04-19 14:57:05 +0100

    first commit

---
+++ coursework/week02/day01/for-loops/src/main/java/Runner.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> students = new ArrayList<String>();
+        students.add("Sujan");
+        students.add("John");
+        students.add("Mack");
+        students.add("Ben");
+
+        System.out.println(students);
+
+    }
+}
